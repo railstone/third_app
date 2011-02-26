c3231263c3320378243a4b4a5774ca2fe886ac37
railstone <mark26acc@gmail.com>
2011-02-26 14:16:21 +0100

    Thirdee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '3564e73242ce83146d8f536a9a3f857849638e50b680785bb068eb38f3e21f2a5e0a1ac94ec0ff35fcc6fbf0bd8d4bb57372e2c4506adadf5badffb58e17b10f'
